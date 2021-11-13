1975c9743fdcde49cb1892f6d8fc9fe6ab1f6799
Antonio Salmeron <93031998+amsalmeron@users.noreply.github.com>
2021-11-13 12:01:26 -0800

    Update annotations.rb

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -33,19 +33,28 @@ build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 # FizzBuzz
 
+# Create a method called fizzbuzz using 3 arguments
 def fizzbuzz(num_1, num_2, range)
+  
+  # Set paremeters for i
   (1..range).each do |i|
+    
+    # Call if statement to meausre when both num_1 and num_2 have modulus zero (no remainder) to print 'fizzbuzz'
     if i % num_1 === 0 && i % num_2 === 0
       puts 'fizzbuzz'
+    # If modulus num_2 is not 0, at least print 'fizz' for num_1
     elsif i % num_1 === 0
       puts 'fizz'
+    # If modulus num_1 is not 0, at least print 'buzz' for num_2
     elsif i % num_2 === 0
       puts 'buzz'
+    # If modulus mum_1 annd modulus num_2 is not 0, at least print the 'i' value that the statment used
     else
       puts i
     end
   end
 end
 
+# Check status of potential fizzbuzz for given imputs
 fizzbuzz(3, 5, 100)
 fizzbuzz(5, 8, 400)
